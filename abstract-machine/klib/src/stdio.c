9b6c62a7ffe7d7b642e95aaa43df79ec80b10502
ysyx_23060192 <577045714@qq.com>
2024-05-09 20:56:35 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard a48416ef8179faa6e40df1355cd9c613af8382f6 fix compile error with clang in MacOS

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,32 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+void int_char(uint32_t x,char *str){	//实现int到char的转换,存放在str里
+	uint32_t tmp = x;
+	uint32_t x_index = 0;	//表示x的位数
+	while(tmp){
+		tmp /= 10;
+		x_index++;	//求x的位数
+	}
+	// memset(str,0,x_index);
+	uint32_t i = x_index;
+	uint32_t n = i;
+	x_index = 1;
+	while(i){
+		x_index *= 10;
+		i--;
+	}
+	x_index /= 10;
+	while(n){ 	
+		tmp = x / x_index;
+		str[i++] = tmp + '0';
+		x %= x_index;
+		x_index /= 10;
+		n--;
+	}
+  str[i] = '\0';
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +40,51 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  char * s;
+  int d;
+  int num = 0;    //记录out中插入了多少字符
+  int si = 0;
+  char str[500] = "\0";   //用来存放int转换成char的字符串
+  va_start(ap,fmt);
+  while(*fmt != '\0'){
+     *out = *fmt;
+     if(*fmt == '%'){
+      fmt++;
+      switch(*fmt){
+        case 's':
+          s = va_arg(ap,char *);
+          while(s[si] != '\0'){
+            *out = s[si];
+            out++;
+            num++;
+            si++;
+            }
+          si = 0;
+          *out = '\0';
+          break;
+
+        case 'd':
+          d = va_arg(ap,int);
+          int_char(d,str);
+          for(int i = 0; str[i] != '\0'; i++){
+            *out = str[i];
+            out++;
+            num++;
+          }
+          *out = '\0';
+          break;
+      }
+      out--;
+      num--;
+    }
+      fmt++;
+      out++;
+      num++;
+  }
+  va_end(ap);
+  *out = '\0';
+  return num;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,131 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t len = 0;
+  for(int i = 0; s[i] != '\0'; i++){
+    len++;
+  }
+    return len;
+  // panic("Not implemented");
 }
 
+//注意： strcpy() 函数不会检查目标字符串的大小是否足够容纳源字符串
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char tmp = src[0];
+  int i;
+  for(i = 0; tmp != '\0'; i++){
+    tmp = src[i];
+    dst[i] = tmp;
+  }
+  return dst;
+  // panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char tmp = src[0];
+  for(int i = 0; i < n; i++){
+    tmp = src[i];
+    dst[i] = tmp;
+  }
+  return dst;
+  // panic("Not implemented");
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  int dstlen = strlen(dst);
+  int srclen = strlen(src);
+  char tmp = src[0];
+  for(int i = dstlen; i < dstlen + srclen; i++){
+    tmp = src[i-dstlen];
+    dst[i] = tmp;
+  }
+  return dst;
+  // panic("Not implemented"); 
 }
+
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int i;
+  for(i = 0; s1[i] != '\0' && s2[i] != '\0'; i++){
+    if(s1[i] < s2[i]) return -1;
+    else if(s1[i] > s2[i]) return 1;
+  }
+  if(s1[i] == '\0' && s2[i] == '\0') return 0;
+  else if(s1[i] != '\0') return 1;
+  else return -1;
+  // panic("Not implemented");
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+int strncmp(const char *s1, const char *s2, size_t n) {   //比较前n个字符
+  int i;
+  for(i = 0; i < n; i++){
+    if(s1[i] < s2[i]) return -1;
+    else if(s1[i] > s2[i]) return 1;
+  }
+  if(s1[i] == s2[i]) return 0;
+  else if(s1[i] != '\0') return 1;
+  else return -1;
+  // panic("Not implemented");
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  assert(s != NULL);
+
+  void *ret = s;
+
+  while(n>0){
+    *(char*)s = c;   //内存是以字节为单位存储的，故要强转成char类型
+    (char*)s++;
+    n--;
+  }
+  return ret;
+  // panic("Not implemented");
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+void *memmove(void *dst, const void *src, size_t n) { //内存块重叠问题解决
+  assert(dst != NULL);
+  assert(src != NULL);
+  void *rst = dst;
+  if(dst < src){  //从左往右复制
+    while(n>0){
+      *(char*)src = *(char*)dst;
+      (char*)src++;
+      (char*)dst++;
+      n--;
+    }
+  }else{
+    while(n>0){
+      *(char*)src = *(char*)dst;
+      (char*)src--;
+      (char*)dst--;
+      n--; 
+    }
+  }
+  return rst;
+  // panic("Not implemented");
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  void *ret = out;
+  while(n>0){
+    *(char*)out = *(char*)in;
+    (char*)out++;
+    (char*)in++;
+    n--;
+  }
+  return ret;
+  // panic("Not implemented");
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+int memcmp(const void* s1, const void* s2, size_t n) {
+  int ret = 0;
+  char* s1_p = (char*)s1;
+  char* s2_p = (char*)s2;
+  for (int i = 0; i < n;i++) {
+    ret = *(s1_p++) - *(s2_p++);// 逐个比较，不相等就退出
+    if (ret != 0)
+      break;
+  }
+  return ret;
 }
-
 #endif
